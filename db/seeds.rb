132f986073732277e7d6e0c692b04f8d168fa9d2
Tristan Dunn <hello@tristandunn.com>
2026-03-30 23:00:58 -0400

    Add transactions to seeds and fix values for consistent calculations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,33 @@
 date = Date.current.beginning_of_month
 
 Budget.first_or_create!.tap do |budget|
-  budget.accounts.find_or_create_by!(name: "Checking") do |account|
+  checking = budget.accounts.find_or_create_by!(name: "Checking") do |account|
     account.balance = 250_000
   end
-  budget.accounts.find_or_create_by!(name: "Savings") do |account|
+
+  savings = budget.accounts.find_or_create_by!(name: "Savings") do |account|
     account.balance = 100_000
   end
-  budget.accounts.find_or_create_by!(name: "United Club") do |account|
+
+  united_club = budget.accounts.find_or_create_by!(name: "United Club") do |account|
     account.balance = -45_000
     account.credit  = true
   end
 
   budget.categories.find_or_create_by(name: Category::INFLOW, position: 0).tap do |parent|
-    parent.subcategories.find_or_create_by(budget: budget, name: Category::AVAILABLE_TO_ASSIGN, position: 0)
+    available_to_assign = parent.subcategories.find_or_create_by(budget: budget, name: Category::AVAILABLE_TO_ASSIGN,
+                                                                 position: 0)
+
+    budget.transactions.find_or_create_by!(account: checking, payee: "Opening Balance") do |transaction|
+      transaction.subcategory = available_to_assign
+      transaction.amount      = 350_000
+      transaction.date        = date
+    end
+    budget.transactions.find_or_create_by!(account: savings, payee: "Opening Balance") do |transaction|
+      transaction.subcategory = available_to_assign
+      transaction.amount      = 100_000
+      transaction.date        = date
+    end
   end
 
   budget.categories.find_or_create_by(name: "Immediate Obligations", position: 1).tap do |parent|
@@ -24,12 +38,13 @@ Budget.first_or_create!.tap do |budget|
       snapshot.amount_used     = 100_000
     end
 
-    parent.subcategories.find_or_create_by(budget: budget, name: "Rent", position: 0).tap do |category|
+    rent = parent.subcategories.find_or_create_by(budget: budget, name: "Rent", position: 0).tap do |category|
       category.snapshots.find_or_create_by!(budget: budget, date: date) do |snapshot|
         snapshot.amount_assigned = 100_000
         snapshot.amount_used     = 100_000
       end
     end
+
     parent.subcategories.find_or_create_by(budget: budget, name: "Water", position: 1).tap do |category|
       category.snapshots.find_or_create_by!(budget: budget, date: date) do |snapshot|
         snapshot.amount_assigned = 5_000
@@ -40,26 +55,47 @@ Budget.first_or_create!.tap do |budget|
         snapshot.amount_assigned = 7_500
       end
     end
+
+    budget.transactions.find_or_create_by!(account: checking, payee: "Landlord") do |transaction|
+      transaction.subcategory = rent
+      transaction.amount      = -100_000
+      transaction.date        = date
+    end
   end
 
   budget.categories.find_or_create_by(name: "Food & Drink", position: 2).tap do |parent|
     parent.snapshots.find_or_create_by!(budget: budget, date: date) do |snapshot|
       snapshot.amount_assigned = 45_000
-      snapshot.amount_used     = 17_500
+      snapshot.amount_used     = 45_000
     end
 
-    parent.subcategories.find_or_create_by(budget: budget, name: "Groceries", position: 0).tap do |category|
+    groceries = parent.subcategories.find_or_create_by(budget: budget, name: "Groceries",
+                                                       position: 0).tap do |category|
       category.snapshots.find_or_create_by!(budget: budget, date: date) do |snapshot|
         snapshot.amount_assigned = 30_000
+        snapshot.amount_used     = 27_500
       end
     end
-    parent.subcategories.find_or_create_by(budget: budget, name: "Dining Out", position: 1).tap do |category|
+
+    dining_out = parent.subcategories.find_or_create_by(budget: budget, name: "Dining Out",
+                                                        position: 1).tap do |category|
       category.snapshots.find_or_create_by!(budget: budget, date: date) do |snapshot|
         snapshot.amount_assigned = 15_000
         snapshot.amount_used     = 17_500
       end
     end
+
+    budget.transactions.find_or_create_by!(account: united_club, payee: "Whole Foods") do |transaction|
+      transaction.subcategory = groceries
+      transaction.amount      = -27_500
+      transaction.date        = date
+    end
+    budget.transactions.find_or_create_by!(account: united_club, payee: "Olive Garden") do |transaction|
+      transaction.subcategory = dining_out
+      transaction.amount      = -17_500
+      transaction.date        = date
+    end
   end
 
-  budget.update!(available_to_assign: 147_500)
+  budget.update!(available_to_assign: 292_500)
 end
