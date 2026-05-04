4bcda10057b3434fdca66b4b0a88ecbfbbcfd2fa
Tristan Dunn <hello@tristandunn.com>
2026-05-03 20:27:36 -0400

    Disable animations in Chrome tests to prevent flaky tests.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,4 +3,10 @@
 require "capybara"
 
 Capybara.server = :puma, { Silent: true }
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.javascript_driver = :selenium_chrome_headless_reduced_motion
+
+Capybara.register_driver :selenium_chrome_headless_reduced_motion do |app|
+  driver = Capybara.drivers[:selenium_chrome_headless].call(app)
+  driver.options[:options].add_argument("--force-prefers-reduced-motion")
+  driver
+end
