f459eeebc00ed8fc511a5a26c9b11802553cf964
Tristan Dunn <hello@tristandunn.com>
2026-08-15 18:15:02 -0400

    Enable a nonce-based content security policy.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   config.action_view.annotate_rendered_view_with_filenames = true
 
+  # Report content security policy violations without enforcing them.
+  config.content_security_policy_report_only = true
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
